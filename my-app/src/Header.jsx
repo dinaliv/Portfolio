6adde31df1b36fd1719a0ac0df3c3afbbc11d5c6
dinaliv <diana.nalivaiko@outlook.com>
2025-11-11 14:54:32 -0800

    nav bar added

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -3,6 +3,13 @@ function Header(){
     return(
         <header>
             <h1>Diana Nalivaiko</h1>
+            <nav>
+                <ul>
+                    <li><a href="">3D</a></li>
+                    <li><a href="">Traditional Art</a></li>
+                    <li><a href="">Photogtaphy</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
