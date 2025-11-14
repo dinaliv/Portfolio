a4e912bde7a17fc9987aea110620fa7b904330ff
dinaliv <diana.nalivaiko@outlook.com>
2025-11-13 21:08:57 -0800

    footer added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   
 
   return (
-    <Header></Header>
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+    
   );
 }
 
---
+++ my-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Diana Nalivaiko</p>
+        </footer>
+    );
+
+   
+}
+
+export default Footer
