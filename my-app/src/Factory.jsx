6e35e78177910717fc0c6d861dd33185e796ca4c
dinaliv <diana.nalivaiko@outlook.com>
2025-11-17 10:13:24 -0800

    fixed shading

--- my-app/src/Factory.jsx
+++ my-app/src/Factory.jsx
@@ -1,24 +1,27 @@
 import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";   
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { TextureLoader, MeshBasicMaterial } from "three";
+import * as THREE from "three"
 
 function FactoryModel() {
   // Load OBJ model
   const obj = useLoader(OBJLoader, "/factory/factory.obj");
 
   // Load texture
-  const texture = useLoader(TextureLoader, "/factory/factory_diffuse.png");
+  const texture = useLoader(THREE.TextureLoader, "/factory/factory_diffuse.png");
 
+  texture.colorSpace = THREE.SRGBColorSpace;                 // correct colors
+  texture.flipY = true;  
+  
   // Apply texture to all child meshes
   obj.traverse((child) => {
     if (child.isMesh) {
       // Use Basic material for flat, cartoon-like appearance
-      child.material = new MeshBasicMaterial({
+      child.material = new THREE.MeshBasicMaterial({
         map: texture,
-        
+        transparent: true,
       });
-      
+      child.material.needsUpdate = true;
     }
   });
 
@@ -28,7 +31,10 @@ function FactoryModel() {
 export default function Factory() {
   return (
     <div className="w-full h-[500px]">
-      <Canvas camera={{ position: [6, 6, -10], fov: 20 }}>
+      <Canvas 
+        camera={{ position: [6, 6, -10], fov: 20 }}
+        gl={{ outputColorSpace: THREE.SRGBColorSpace }} 
+      >
         
 
         <FactoryModel />
